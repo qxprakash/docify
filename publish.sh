ed3193a4041f05b1bf56e07569597e097d50d702
Prakash <qxprakash@gmail.com>
2024-10-25 23:56:22 +0530

    working

--- publish.sh
+++ publish.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -ex
 cargo build --workspace
-cargo test --workspace
-cargo doc --example integration
-cargo fmt -- --check
-cargo doc --workspace --all-features
 
 cd macros
 cargo publish
